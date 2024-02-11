e01b89c069481439c56ab39dd489482e610477f2
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-11 16:32:30 +0200

    JS Data Types

---
+++ 14.0 Javascript/02-jsDataTypes.js
@@ -0,0 +1,7 @@
+var isOver = true;
+var age = 5;
+var name = "Stamat";
+
+console.log(Object.keys({isOver})[0] + " is of type " + typeof(isOver));
+console.log(Object.keys({age})[0] + " is of type " + typeof(age));
+console.log(Object.keys({name})[0] + " is of type " + typeof(name));
