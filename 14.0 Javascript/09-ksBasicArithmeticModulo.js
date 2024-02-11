a0a24240107962c5b7ccb9d6a610575af6145497
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-11 20:21:15 +0200

    JS More Math Operations

--- 14.0 Javascript/09-ksBasicArithmeticModulo.js
+++ 14.0 Javascript/09-ksBasicArithmeticModulo.js
@@ -3,3 +3,15 @@ var num = 8%5; // 3
 var dogAge = 7;
 var humanAge = (dogAge - 2) * 4 + 21;
 console.log(humanAge);
+
+
+var num1 = 15;
+num1 /= 5;
+var num2 = 4;
+num2 *= 3;
+
+num1++;
+num2--;
+
+console.log(num1);
+console.log(num2);
