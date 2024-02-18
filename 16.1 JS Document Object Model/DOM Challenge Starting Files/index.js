37a994c53c5fed833d3111508cafab50d14156d7
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-18 12:03:48 +0200

    JS DOM Manipulation

--- 16.1 JS Document Object Model/DOM Challenge Starting Files/index.js
+++ 16.1 JS Document Object Model/DOM Challenge Starting Files/index.js
@@ -1,1 +1,31 @@
-document.querySelector("input".click());
+document.querySelector("input").click();
+document.querySelector("ul").lastElementChild.innerHTML = "third modified";
+var items1 = document.getElementsByTagName("li");
+ document.getElementsByTagName("li")[2].style.color = "orange";
+var btn1 = document.getElementById("123");
+var btn2 = document.getElementsByClassName("123");
+var btn3 = document.getElementsByName("123");
+var btn4 = document.getElementsByTagName("123");
+// var btn5 = document.getElementsByTagNameNS("123");
+var title = document.querySelector("#title");
+var btn6 = document.querySelector(".btn");
+var h1 = document.querySelector("h1");
+var anchorInListItem = document.querySelector("li a");
+var listItemWithClassItem = document.querySelector("li.item");
+document.querySelector("li a").style.color = "green";
+document.querySelector("button").style.background = "yellow";
+
+// something.doS omething(); method
+// something.something = something; assign property
+// something.something; access property
+
+// prop:
+// innerHTML
+// style
+// firstChild
+// lastChild
+
+// Method / func
+// click();
+// appendChild();
+// SetAttribute();
