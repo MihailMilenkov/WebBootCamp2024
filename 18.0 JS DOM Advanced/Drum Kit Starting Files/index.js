9166e49b25eaba1a95b6c1c03fa5e84c713729ed
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-25 15:18:44 +0200

    JS Click To Play Sound

---
+++ 18.0 JS DOM Advanced/Drum Kit Starting Files/index.js
@@ -0,0 +1,89 @@
+var document = "index.html";
+var drumButtonCount = document.querySelectorAll(".drum").length;
+
+// for (var i = 0; i < drumButtonCount; i++) {
+//     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);    
+// }
+document.querySelectorAll(".drum")[0].addEventListener("click", handleClickW);  
+document.addEventListener('keydown', (event) => {    
+    if (event.keyCode === 87) {
+        handleClickW();
+    }
+    
+    //alert(event.keyCode);
+    // do something
+  }); 
+document.querySelectorAll(".drum")[1].addEventListener("click", handleClickA);  
+document.querySelectorAll(".drum")[2].addEventListener("click", handleClickS);  
+document.querySelectorAll(".drum")[3].addEventListener("click", handleClickD);  
+document.querySelectorAll(".drum")[4].addEventListener("click", handleClickJ);  
+document.querySelectorAll(".drum")[5].addEventListener("click", handleClickK);  
+document.querySelectorAll(".drum")[6].addEventListener("click", handleClickL);  
+
+function handleClickW(){
+    createAndPlay("w");
+}
+function handleClickA(){
+    createAndPlay("a");
+}
+function handleClickS(){
+    createAndPlay("s");
+}
+function handleClickD(){
+    createAndPlay("d");
+}
+function handleClickJ(){
+    createAndPlay("j");
+}
+function handleClickK(){
+    createAndPlay("k");
+}
+function handleClickL(){
+    createAndPlay("l");
+}
+
+function createAndPlay(button){
+    switch (button) {
+        case "w":
+            var audio = new Audio("./sounds/crash.mp3")
+            break;
+            case "a":
+                var audio = new Audio("./sounds/kick-bass.mp3")
+                break;
+                case "s":
+                    var audio = new Audio("./sounds/snare.mp3")
+                    break;
+                    case "d":
+                        var audio = new Audio("./sounds/tom-1.mp3")
+                        break;
+                        case "j":
+                            var audio = new Audio("./sounds/tom-2.mp3")
+                            break;
+                            case "k":
+                                var audio = new Audio("./sounds/tom-3.mp3")
+                                break;
+                                case "l":
+                                    var audio = new Audio("./sounds/tom-4.mp3")
+                                    break;    
+        default:
+            break;
+    }
+    
+    audio.play();
+}
+
+function calculator(num1, num2, operator){
+    return operator(num1, num2);
+}
+function add(num1, num2){
+    return num1 + num1;
+}
+function subtract(num1, num2){
+    return num1 - num1;
+}
+function multiply(num1, num2){
+    return num1 * num1;
+}
+function divide(num1, num2){
+    return num1 / num1;
+}
