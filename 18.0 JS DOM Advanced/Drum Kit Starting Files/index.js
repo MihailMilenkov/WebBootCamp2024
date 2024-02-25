516907c9b1001f8bc0de573c2ce5f41e35f7f425
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-25 19:45:02 +0200

    JS Add / Remove CSS Classes

--- 18.0 JS DOM Advanced/Drum Kit Starting Files/index.js
+++ 18.0 JS DOM Advanced/Drum Kit Starting Files/index.js
@@ -2,29 +2,42 @@ document.addEventListener('keyup', (event) => {
     switch (event.keyCode) {
         case 87:
             var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            buttonAnimation(event.key);
             break;
             case 65:
                 var audio = new Audio("./sounds/kick-bass.mp3");
+                audio.play();
+                buttonAnimation(event.key);
                 break;
                 case 83:
                     var audio = new Audio("./sounds/snare.mp3");
+                    audio.play();
+                    buttonAnimation(event.key);
                     break;
                     case 68:
                         var audio = new Audio("./sounds/tom-1.mp3");
+                        audio.play();
+                        buttonAnimation(event.key);
                         break;
                         case 74:
                             var audio = new Audio("./sounds/tom-2.mp3");
+                            audio.play();
+                            buttonAnimation(event.key);
                             break;
                             case 75:
                                 var audio = new Audio("./sounds/tom-3.mp3");
+                                audio.play();
+                                buttonAnimation(event.key);
                                 break;
                                 case 76:
                                     var audio = new Audio("./sounds/tom-4.mp3");
+                                    audio.play();
+                                    buttonAnimation(event.key);
                                     break;    
         default:
             break;
-    }     
-    audio.play();
+        }
 }); 
 
 var drumButtonCount = document.querySelectorAll(".drum").length;
@@ -34,32 +47,53 @@ for (var i = 0; i < drumButtonCount; i++) {
         var btnInnerHtml = this.innerHTML;
         switch (btnInnerHtml) {
             case "w":
-                var audio = new Audio("./sounds/crash.mp3");        
+                var audio = new Audio("./sounds/crash.mp3");   
+                audio.play();
+                buttonAnimation(btnInnerHtml);       
                 break;
                 case "a":
                     var audio = new Audio("./sounds/kick-bass.mp3");
+                    audio.play();
+                    buttonAnimation(btnInnerHtml);  
                     break;
                     case "s":
                         var audio = new Audio("./sounds/snare.mp3");
+                        audio.play();
+                        buttonAnimation(btnInnerHtml);  
                         break;
                         case "d":
                             var audio = new Audio("./sounds/tom-1.mp3");
+                            audio.play();  
+                            buttonAnimation(btnInnerHtml);
                             break;
                             case "j":
                                 var audio = new Audio("./sounds/tom-2.mp3");
+                                audio.play();  
+                                buttonAnimation(btnInnerHtml);
                                 break;
                                 case "k":
                                     var audio = new Audio("./sounds/tom-3.mp3");
+                                    audio.play();  
+                                    buttonAnimation(btnInnerHtml);
                                     break;
                                     case "l":
                                         var audio = new Audio("./sounds/tom-4.mp3");
+                                        audio.play();  
+                                        buttonAnimation(btnInnerHtml);
                                         break;    
             default:
-                console.log(btnInnerHtml)
                 break;
-        }
-        audio.play();     
+        }   
     });
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(() => {        
+    activeButton.classList.remove("pressed");
+    }, 100);
 }
 
 function calculator(num1, num2, operator){
