5d32424506fcf9e14c67ce078baf4d14f80260dc
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-25 16:18:05 +0200

    JS Switch On InnerHTML

--- 18.0 JS DOM Advanced/Drum Kit Starting Files/index.js
+++ 18.0 JS DOM Advanced/Drum Kit Starting Files/index.js
@@ -1,95 +1,65 @@
-var document = "index.html";
-var drumButtonCount = document.querySelectorAll(".drum").length;
-
-// for (var i = 0; i < drumButtonCount; i++) {
-//     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);    
-// }
 document.addEventListener('keyup', (event) => {    
     switch (event.keyCode) {
         case 87:
-            handleClickW();
+            var audio = new Audio("./sounds/crash.mp3");
             break;
             case 65:
-                handleClickA();
+                var audio = new Audio("./sounds/kick-bass.mp3");
                 break;
                 case 83:
-                    handleClickS();
+                    var audio = new Audio("./sounds/snare.mp3");
                     break;
                     case 68:
-                        handleClickD();
+                        var audio = new Audio("./sounds/tom-1.mp3");
                         break;
                         case 74:
-                            handleClickJ();
+                            var audio = new Audio("./sounds/tom-2.mp3");
                             break;
                             case 75:
-                                handleClickK();
+                                var audio = new Audio("./sounds/tom-3.mp3");
                                 break;
                                 case 76:
-                                    handleClickL();
+                                    var audio = new Audio("./sounds/tom-4.mp3");
                                     break;    
         default:
             break;
-    }    
-    // alert(event.keyCode);
-  }); 
-document.querySelectorAll(".drum")[0].addEventListener("click", handleClickW);  
-document.querySelectorAll(".drum")[1].addEventListener("click", handleClickA);  
-document.querySelectorAll(".drum")[2].addEventListener("click", handleClickS);  
-document.querySelectorAll(".drum")[3].addEventListener("click", handleClickD);  
-document.querySelectorAll(".drum")[4].addEventListener("click", handleClickJ);  
-document.querySelectorAll(".drum")[5].addEventListener("click", handleClickK);  
-document.querySelectorAll(".drum")[6].addEventListener("click", handleClickL);  
-
-function handleClickW(){
-    createAndPlay("w");
-}
-function handleClickA(){
-    createAndPlay("a");
-}
-function handleClickS(){
-    createAndPlay("s");
-}
-function handleClickD(){
-    createAndPlay("d");
-}
-function handleClickJ(){
-    createAndPlay("j");
-}
-function handleClickK(){
-    createAndPlay("k");
-}
-function handleClickL(){
-    createAndPlay("l");
-}
-
-function createAndPlay(button){
-    switch (button) {
-        case "w":
-            var audio = new Audio("./sounds/crash.mp3")
-            break;
-            case "a":
-                var audio = new Audio("./sounds/kick-bass.mp3")
-                break;
-                case "s":
-                    var audio = new Audio("./sounds/snare.mp3")
-                    break;
-                    case "d":
-                        var audio = new Audio("./sounds/tom-1.mp3")
-                        break;
-                        case "j":
-                            var audio = new Audio("./sounds/tom-2.mp3")
-                            break;
-                            case "k":
-                                var audio = new Audio("./sounds/tom-3.mp3")
-                                break;
-                                case "l":
-                                    var audio = new Audio("./sounds/tom-4.mp3")
-                                    break;    
-        default:
-            break;
-    }
-    
+    }     
     audio.play();
+}); 
+
+var drumButtonCount = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < drumButtonCount; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var btnInnerHtml = this.innerHTML;
+        switch (btnInnerHtml) {
+            case "w":
+                var audio = new Audio("./sounds/crash.mp3");        
+                break;
+                case "a":
+                    var audio = new Audio("./sounds/kick-bass.mp3");
+                    break;
+                    case "s":
+                        var audio = new Audio("./sounds/snare.mp3");
+                        break;
+                        case "d":
+                            var audio = new Audio("./sounds/tom-1.mp3");
+                            break;
+                            case "j":
+                                var audio = new Audio("./sounds/tom-2.mp3");
+                                break;
+                                case "k":
+                                    var audio = new Audio("./sounds/tom-3.mp3");
+                                    break;
+                                    case "l":
+                                        var audio = new Audio("./sounds/tom-4.mp3");
+                                        break;    
+            default:
+                console.log(btnInnerHtml)
+                break;
+        }
+        audio.play();     
+    });
 }
 
 function calculator(num1, num2, operator){
