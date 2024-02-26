de926e5b6dcfb5bb01d2f5e95bda3fb09262f0e5
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-26 14:40:12 +0200

    JQuery Acess Text / Attributes

--- 19.0 JQuery/index.js
+++ 19.0 JQuery/index.js
@@ -14,3 +14,9 @@ $("h1").addClass("big-title margin-50");
 if ($("h1").hasClass("margin-50")) {
     $("h1").removeClass("margin-50");
 }
+
+$("button").text("Click Fast");
+// $("button").html("<em>Hey</em>");
+
+console.log($("a").attr("href"));
+console.log($("a").attr("class"));
