52644f448dc9126990eefcf7d29152bba51aaeab
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-26 20:43:15 +0200

    Add RandomButton PlaySound

--- 20.0 Simon Game/game.js
+++ 20.0 Simon Game/game.js
@@ -1,1 +1,17 @@
-alert("dsad")
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var randomChosenColour = buttonColours[nextSequence()];
+$("." + randomChosenColour).fadeOut(100).fadeIn(100)
+
+var soundPath = `./sounds/${randomChosenColour}.mp3`;
+playSound(soundPath);
+
+function nextSequence(){
+    return Math.round(Math.random() * 3);
+}
+function playSound(sound){
+    var audio = new Audio(sound);
+    audio.muted = true;
+    //audio.play();
+}
