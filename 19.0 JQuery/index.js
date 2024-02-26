095b848ee0f58eaeca6a7eb6952fcbbd3a3a3087
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-26 14:23:16 +0200

    JQuery Select Manipulate Check Style

---
+++ 19.0 JQuery/index.js
@@ -0,0 +1,16 @@
+//document.querySelector();
+// jQuery();
+
+//document.querySelector("h1");
+// $(document).ready(function() {
+//     $("h1").css("color", "green");
+// });
+
+//document.querySelectorAll("button");
+$("button");
+
+$("h1").addClass("big-title margin-50");
+
+if ($("h1").hasClass("margin-50")) {
+    $("h1").removeClass("margin-50");
+}
