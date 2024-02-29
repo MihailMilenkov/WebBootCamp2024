c6d650c8477990e0b76374830e5c52201aa5e9eb
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-02-29 13:29:15 +0200

    [Simon Game] Click Button

--- 20.0 Simon Game/game.js
+++ 20.0 Simon Game/game.js
@@ -1,17 +1,28 @@
-var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
 
-var randomChosenColour = buttonColours[nextSequence()];
-$("." + randomChosenColour).fadeOut(100).fadeIn(100)
+$(document).keypress(function(event) { // event.key
+     nextSequence();
+});
 
-var soundPath = `./sounds/${randomChosenColour}.mp3`;
-playSound(soundPath);
+$(".btn").click(function(e){
+    console.log(this.id);
+    //console.log(e.currentTarget.id);
+    //userClickedPattern.push(e.currentTarget.id);
+    userClickedPattern.push(this.id);
+});
 
 function nextSequence(){
-    return Math.round(Math.random() * 3);
+    var randomColorIndex =  Math.round(Math.random() * 3);
+
+    var randomChosenColour = buttonColours[randomColorIndex];
+    gamePattern.push(randomChosenColour);
+    $("." + randomChosenColour).fadeOut(100).fadeIn(100)
+
+    playSound(`./sounds/${randomChosenColour}.mp3`);
 }
 function playSound(sound){
     var audio = new Audio(sound);
-    audio.muted = true;
-    //audio.play();
+    audio.play();
 }
