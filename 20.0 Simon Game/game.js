d2bf394f822b38973d00648c109d6872781542f3
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-03-03 13:16:52 +0200

    [SG] Check Input

--- 20.0 Simon Game/game.js
+++ 20.0 Simon Game/game.js
@@ -1,32 +1,56 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var gameIsRunning = false;
+var currentIndexClicked = 0;
+var highScore = 0;
 
 $(document).keypress(function(event) { // event.key
-     nextSequence();
+    if (!gameIsRunning) {
+        $("h1").text("Level " + level);
+        nextSequence();        
+        gameIsRunning = true;        
+    }
 });
 
 $(".btn").click(function(){
-    //console.log(this.id);
-    //console.log(e.currentTarget.id);
-    //userClickedPattern.push(e.currentTarget.id);
     userClickedPattern.push(this.id);
     playSound(`./sounds/${this.id}.mp3`);
     animatePress(this.id);
+    checkAnswer(this.id);
 });
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentIndexClicked] == currentLevel) { // check if last click is correct
+        if(gamePattern.length == userClickedPattern.length){
+
+            console.log("success");
+        }
+        currentIndexClicked++;
+    }else{
+        playSound(`./sounds/wrong.mp3`);
+        level = 0;
+        gameIsRunning = false;
+        currentIndexClicked = 0;
+        highScore = gamePattern.length;
+        console.log("wrong");
+    }    
+}
 
 function animatePress(currentColor){    
     var btn = $("#" + currentColor);
-    
+
     $(btn).addClass("pressed");
 
     setTimeout(() => {
         $(btn).removeClass("pressed");
     }, 100);
-    console.log(1);
 }
 
 function nextSequence(){
+    level ++;
+    $("h1").text("Level " + level);
     var randomColorIndex =  Math.round(Math.random() * 3);
 
     var randomChosenColour = buttonColours[randomColorIndex];
