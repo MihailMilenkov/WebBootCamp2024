5b8bd920f5b993f53fb5a0481d52a6dca827d96d
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-03-04 22:23:14 +0200

    Using Node.js

---
+++ 21.0 Unix Command Line/2.1 Using Node/index.js
@@ -0,0 +1,16 @@
+// node.js repl(read evaluate print loop) commands
+//.help
+//.break    Sometimes you get stuck, this gets you out
+//.clear    Alias for .break
+//.editor   Enter editor mode
+//.exit     Exit the REPL
+//.help     Print this help message
+//.load     Load JS from a file into the REPL session
+//.save     Save all evaluated commands in this REPL session to a file
+
+// node - starts node
+// shebang - #!/usr/bin/node - put on first line of a file to tell the os to run this file in node
+// require("./index.js") - open file index.js if in node
+// node index.js - open file with index.js
+
+console.log("printing with node");
