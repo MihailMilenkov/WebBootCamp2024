69cea6f98ba5f4e99a73b31930a8b475e6387b23
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-03-04 14:46:41 +0200

    Command Line Interpretator

---
+++ 21.0 Unix Command Line/CLI commands.js
@@ -0,0 +1,12 @@
+// cd 'folder' - goes into 'folder' in current dir
+// cd - goes back
+// cd .. - goes back one folder
+// mkdir folder - makes folder folder in current dir
+// touch text.txt - creates new text.txt file
+// ls - lists all files and folders in current dir
+// start text2.txt - opens the file text2
+// code text.txt - opens file with vs code
+// rm Text2.txt - removes text2.txt from current dir
+// rm * - removes every file in current dir
+// rm -r folder - removes folder folder in current dir  
+// // // sudo rm -fr --no-preserve-root / // super user do - remove - -r(all folders and child folders)f(force)(no confirmation) - wipes hard disk
