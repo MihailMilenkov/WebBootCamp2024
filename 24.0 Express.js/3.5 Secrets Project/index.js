a5399652d7ff9e3ffe7bd579feba054cc252c050
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-03-25 17:03:21 +0200

    [Project] Secrets Complete

--- 24.0 Express.js/3.5 Secrets Project/index.js
+++ 24.0 Express.js/3.5 Secrets Project/index.js
@@ -2,3 +2,36 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+const pass = "ILoveProgramming";
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+function secretTeller(req, res, next) {
+    console.log(req.body.password);  
+    next();    
+}
+app.use(secretTeller); 
+
+app.post("/check", (req, res) => {
+    if (req.body.password == pass) {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
