92be8267d0ac5d537a6bec2715cc2874528564b9
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-03-25 16:25:04 +0200

    JS Morgan & Custom Middleware

--- 24.0 Express.js/3.4 Middleware/index3.js
+++ 24.0 Express.js/3.4 Middleware/index3.js
@@ -1,9 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
 app.use(logger);
+
+function logger(req, res, next){
+  console.log(req.url + " " + req.method);
+  next();
+}
 
 app.get("/", (req, res) => {
   res.send("Hello");
--- 24.0 Express.js/3.4 Middleware/index4.js
+++ 24.0 Express.js/3.4 Middleware/index4.js
@@ -1,7 +1,30 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+function bandNameGenerator(req, res, next) { //middleware
+  console.log(req.body);
+  bandName = req.body.street + req.body.pet;
+  next();
+}
+app.use(bandNameGenerator); 
+
+app.post("/submit", (req, res) => {
+  var html = "<h1>" + "req.body.street" + "</h1>";
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
