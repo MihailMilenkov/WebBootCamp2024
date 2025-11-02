0ef597d22c4bd3d763859bf69e1e90c9e9b64bad
Mihail Milenkov <mihailmilenkov681@gmail.com>
2025-11-02 13:40:02 +0200

    EJS Exercise

--- 24.0 Express.js/3.4 Middleware/index1.js
+++ 24.0 Express.js/3.4 Middleware/index1.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
-  // console.log(__dirname + "/public/index.html");
+   //console.log(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
